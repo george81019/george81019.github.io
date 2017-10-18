49ca8d2d54c7ed25c8a9fceabc50ea3649958404
george81019 <george81019@gmail.com>
2017-10-18 14:08:19 +0800

    dinamic version

---
+++ dinamic.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".menuitem1").click(function(){
+        $("#Am").fadeToggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".menuitem2").click(function(){
+        $("#Fav").fadeToggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".menuitem3").click(function(){
+        $("#Cm").fadeToggle(1000);
+    });
+});
+
+$(document).ready(function(){
+    $(".menuitem4").click(function(){
+	$("#MI").fadeToggle(1000);
+    });
+});
